80d56d71d676833d5d9c0a717cd84f25f7636d9f
mubin.khan <mubin.khan@upaybd.com>
2025-04-09 14:56:37 +0600

    udpate the desktop version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,10 @@ export default function Home() {
       <Header />
       <main className="w-[90vw] mx-auto px-4 flex flex-col-reverse md:flex-row items-center">
         <div className="md:w-1/2 mb-10 md:mb-0 z-10">
-          <h1 className="text-xl md:text-5xl font-bold mb-1 text-center">
+          <h1 className="text-xl md:text-3xl font-bold mb-1 text-center md:text-start">
             Want to Turn Social Media Into a Profitable Career?
           </h1>
-          <h2 className="text-xl text-center md:text-3x mb-6 font-bold text-[#00E7F9] text-shadow-lg text-shadow-[#FC004E]">
+          <h2 className="text-xl text-center md:text-start md:text-3xl mb-6 font-bold text-[#00E7F9] text-shadow-lg text-shadow-[#FC004E]">
             Discover your way to success with Fametonic:
           </h2>
           <div className="space-y-3 mb-8 text-sm">
@@ -33,17 +33,23 @@ export default function Home() {
               <p>Learn from expert-led courses designed for aspiring influencers</p>
             </div>
           </div>
-          <div className="text-xs text-[#a9a9a9] text-center space-y-4 mb-8">
+          <div className="text-xs text-[#a9a9a9] md:hidden text-center space-y-4 mb-8">
             <p>
               By clicking &quot;Get Started&quot;, you agree with Terms and Conditions, Privacy Policy, Subscription Terms
             </p>
             <p>Fametonic 2025 ©All Rights Reserved.</p>
           </div>
           <div className="space-y-2">
-            <button className="bg-[#fc004e] py-3 px-8 rounded-md shadow-md shadow-gray-50 font-bold flex items-center justify-center w-full md:w-auto">
-              GET STARTED <span className="ml-2">›</span>
+            <button className="bg-[#fc004e] py-3 px-8 rounded-md shadow-md shadow-gray-50 font-bold flex items-center justify-center w-full md:w-[300px]">
+              GET STARTED <span className="ml-2 font-bold text-xl">›</span>
             </button>
-            <p className="text-center text-xs md:text-left text-[#a9a9a9]">1-minute quiz for personalized insights</p>
+            <p className="text-center text-xs md:text-center md:w-[300px] mt-1 text-[#a9a9a9]">1-minute quiz for personalized insights</p>
+          </div>
+          <div className="mx-auto mt-10 relative text-xs text-[#a9a9a9] hidden md:block w-[90vw]">
+            <p>Fametonic 2025 ©All Rights Reserved.</p>
+            <p className="mb-2 text-xs text-[#a9a9a9]">
+              By clicking &quot;Get Started&quot;, you agree with Terms and Conditions, Privacy Policy, Subscription Terms
+            </p>
           </div>
         </div>
         <div className="w-full -mt-8 md:w-1/2 relative">
@@ -58,12 +64,6 @@ export default function Home() {
           </div>
         </div>
       </main>
-      <footer className="container mx-auto px-4 py-6 text-xs text-[#a9a9a9] hidden">
-        <p>Fametonic 2025 ©All Rights Reserved.</p>
-        <p className="mb-2 text-xs text-[#a9a9a9]">
-          By clicking &quot;Get Started&quot;, you agree with Terms and Conditions, Privacy Policy, Subscription Terms
-        </p>
-      </footer>
     </div>
   )
 }
